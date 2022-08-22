1ab5e2da7a1b21ca21978434901b91377a2498b7
rob <masterofblockchain20@gmail.com>
2022-08-22 16:28:41 +0530

    update

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ http://127.0.0.1:5500/index.html
 
 127.0.0.1 is a local host
 5500 is a port
+//mine code
+
+<!DOCTYPE html>
+<html lang="" en>
+    <head>
+        <title>fund me app</title>
+    </head>
+    <body>
+        what's up?
+        <script src="./index.js" type="module"></script>
+        <button id="connectbutton">connect</button>
+        <button id="fundbutton">fund</button>
+    </body>
+</html>
--- index.js
+++ index.js
@@ -1,28 +1,84 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
 
-const connecButton = document.getElementById("connectbutton")
-const fundButton = document.getElementById("fundbutton")
+const connectbutton = document.getElementById("connectbutton")
+const fundbutton = document.getElementById("fundbutton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawalButton = document.getElementById("withdrawalButton")
 connectbutton.onclick = connect
 fundbutton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawalButton.onclick = withdrawal
+console.log(ethers)
 
 async function connect() {
     if (typeof window.ethereum != "undefined") {
-        await window.ethereum.request({
+        await ethereum.request({
             method: "eth_requestAccounts",
         })
-        document.getElementById("connect button").innerHTML = "connected!"
+        connectbutton.innerHTML = "connected!"
     } else {
-        document.getElementById("connect button").innerHTML =
-            "please install metamask"
+        connectbutton.innerHTML = "please install metamask"
     }
 }
 
-async function fund(ethAmount) {
-    console.log(`fundin with ${ethAmount}....`)
+async function getBalance() {
+    if (typeof window.ethereum != "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+    console.log(`fundin with ${ethAmount}`)
+
     if (typeof window.ethereum != "undefined") {
         //provider /connection to the blockchain
         //signer /wallet /someone with some gas
         //contract that we are intercating with
         //ABI and ADDRESS
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            //wait for this tx to finish
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("done")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}....`)
+    //liste for this transaction to be finish
+    //return new Prommise()
+    //function listener(){}/ we dont use it since we have annoymous function
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `completed with ${transactionReceipt.confirmations}confirmations`
+            )
+            resolve()
+        })
+    })
+}
+async function withdrawal() {
+    if (typeof window.ethereum != "undefined") {
+        console.log("withdrawaling...")
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdrawal()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
